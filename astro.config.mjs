d7f3147b77e5a9b9f8bcdd00e7ef94f9f663c1a4
Xephyrus <rtx2080titan@163.com>
2025-07-10 00:11:23 +0800

    update blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://xephyrus.netlify.app',
-	integrations: [mdx(), sitemap()],
+  site: "https://xephyrus.netlify.app",
+  integrations: [mdx(), sitemap()],
+  markdown: {
+    shikiConfig: {
+      theme: "vitesse-light",
+    },
+  },
 });
